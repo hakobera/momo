dec81e8a298c19c88a0440fdc3be2073ff17f036
Kazuyuki Honda <hakobera@gmail.com>
2019-12-27 21:36:52 +0900

    test

--- build/raspbian-buster_armv6/armv6.toolchain.cmake
+++ build/raspbian-buster_armv6/armv6.toolchain.cmake
@@ -7,9 +7,9 @@ SET(CMAKE_C_COMPILER /root/llvm/clang/bin/clang)
 SET(CMAKE_CXX_COMPILER /root/llvm/clang/bin/clang++)
 
 SET(CMAKE_SYSROOT /root/rootfs)
-SET(CMAKE_C_FLAGS "-v -D_LIBCPP_ABI_UNSTABLE -nostdinc++ -isystem/root/llvm/libcxx/include --target=arm-linux-gnueabihf -I/root/rootfs/usr/include/arm-linux-gnueabihf")
-SET(CMAKE_CXX_FLAGS "-v -D_LIBCPP_ABI_UNSTABLE -nostdinc++ -isystem/root/llvm/libcxx/include --target=arm-linux-gnueabihf -I/root/rootfs/usr/include/arm-linux-gnueabihf")
-SET(CMAKE_MODULE_LINKER_FLAGS_INIT "-v --target=arm-linux-gnueabihf -L/root/rootfs/usr/lib/arm-linux-gnueabihf")
+SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -v -D_LIBCPP_ABI_UNSTABLE -nostdinc++ -isystem/root/llvm/libcxx/include --target=arm-linux-gnueabihf -I/root/rootfs/usr/include/arm-linux-gnueabihf")
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -v -D_LIBCPP_ABI_UNSTABLE -nostdinc++ -isystem/root/llvm/libcxx/include --target=arm-linux-gnueabihf -I/root/rootfs/usr/include/arm-linux-gnueabihf")
+SET(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -v --target=arm-linux-gnueabihf -L/root/rootfs/usr/lib/arm-linux-gnueabihf")
 
 # where is the target environment
 SET(CMAKE_FIND_ROOT_PATH /root/aws-sdk-cpp)
