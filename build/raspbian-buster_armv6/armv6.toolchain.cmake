4be153c6409a2956e7b3f4df01bd33c3a2cfcd00
Kazuyuki Honda <hakobera@gmail.com>
2019-12-27 20:46:36 +0900

    Clang をつかってみる

--- build/raspbian-buster_armv6/armv6.toolchain.cmake
+++ build/raspbian-buster_armv6/armv6.toolchain.cmake
@@ -3,8 +3,8 @@ SET(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabihf-gcc)
-SET(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++)
+SET(CMAKE_C_COMPILER /root/llvm/clang/bin/clang)
+SET(CMAKE_CXX_COMPILER /root/llvm/clang/bin/clang++)
 
 # where is the target environment
 SET(CMAKE_FIND_ROOT_PATH /root/aws-sdk-cpp)
